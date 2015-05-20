d060ea5db127656748023db2c4f8d1c6efc16f5e
jproyo <jproyo@mxhero.com>
2015-05-19 23:16:23 -0300

    Adding index controller

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
 
+	def init_items
+		user = User.create!(username: "User 1", password: "xxxx")
+  	10.times do |i|
+  		Item.create!(link: "http://youtube.com/SDfafdaf#{i}", link_type: (i%2==0?"Youtube":"Image"), description: "My description #{i}", user: user)
+  	end
+  	user.id
+	end
+
   describe "GET #index" do
-    it "returns http success" do
+    it "returns list of items for an user" do
+    	user_id = init_items
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
     end
   end
 
