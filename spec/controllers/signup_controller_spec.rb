47df48160fa506e6c5d6d58dd0f217658fccfec3
jproyo <jproyo@mxhero.com>
2015-06-02 17:49:11 -0300

    NA: Adding tdd signup

--- spec/controllers/signup_controller_spec.rb
+++ spec/controllers/signup_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe SignupController, type: :controller do
       response.should have_http_status(200)
     end
 
-    it "go to signup page and fill up form" do
+    it "go to signup page and fill up form success" do
       get :index
       assert_template 'signup/index'
 			post :create, signup: { username: 'unknown_user@test.com', password: 'password' }
@@ -19,6 +19,17 @@ RSpec.describe SignupController, type: :controller do
 			refute flash.empty?
 			assert_equal flash.now[:notice], 'User has been created successfully'
     end    
+
+    it "go to signup page and fill up form fail" do
+      get :index
+      assert_template 'signup/index'
+			User.any_instance.stub(:save).and_return(false)
+			post :create, signup: { username: 'unknown_user@test.com', password: 'password' }
+			assert_template 'signup/index'
+			refute flash.empty?
+			assert_equal flash.now[:danger], 'Could not create user'
+    end    
+
   end
 
 end
