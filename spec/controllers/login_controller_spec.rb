fcc38808c90ca996ca674501cac88b19292aa7eb
jproyo <jproyo@mxhero.com>
2015-06-02 16:43:55 -0300

    NA: Adding test for non unexisting user

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe LoginController, type: :controller do
 
+	describe "GET #login index" do
+    it "go to login page" do
+      get :index
+      response.should be_success
+      response.should have_http_status(200)
+    end
+
+    it "login with unexisting user" do
+      get :index
+      assert_template 'login/index'
+			post :create, login: { email: 'unknown_user@test.com', password: 'password' }
+			assert_template 'login/index'
+			assert !flash.empty?
+			assert_equal flash.now[:notice], 'User is not registered. Please click in Signup link'
+    end    
+  end
+
 end
