cdab7919594f5b592672a04f1ce8868e5f7cf8c0
jproyo <jproyo@mxhero.com>
2015-06-02 17:41:51 -0300

    NA: Adding tdd signup

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -5,4 +5,18 @@ class SignupController < ApplicationController
 	def index
 	end
 
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash.now[:notice] = "User has been created successfully"
+		else
+			flash.now[:danger] = "Could not create user"
+		end
+		render :index
+	end
+
+	def user_params
+    params.require(:signup).permit(:username, :password)
+  end
+
 end
--- spec/controllers/signup_controller_spec.rb
+++ spec/controllers/signup_controller_spec.rb
@@ -14,9 +14,10 @@ RSpec.describe SignupController, type: :controller do
     it "go to signup page and fill up form" do
       get :index
       assert_template 'signup/index'
-			post :create, signup: { email: 'unknown_user@test.com', password: 'password' }
-			assert_template 'signup/create'
+			post :create, signup: { username: 'unknown_user@test.com', password: 'password' }
+			assert_template 'signup/index'
 			refute flash.empty?
+			assert_equal flash.now[:notice], 'User has been created successfully'
     end    
   end
 
