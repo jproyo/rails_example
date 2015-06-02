0b1e2da8d4ff4f320554dd4385deaddd76aa4213
jproyo <jproyo@mxhero.com>
2015-06-02 09:33:30 -0300

    #NA: Adding login view

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,11 +5,14 @@ class LoginController < ApplicationController
 
 	def create
  		user = User.find_by(username: params[:login][:email].downcase)
-    if user && user.authenticate(params[:login][:password])
+ 		if user.nil?
+ 			flash.now[:notice] = "User is not registered. Please click in Signup link" 
+ 			render :index
+    elsif user.authenticate(params[:login][:password])
     	add_user user
     	redirect_to @items
     else
-    	flash[:danger] = 'Invalid email/password combination'
+    	flash.now[:danger] = 'Invalid email/password combination'
 			render :index
 		end
 	end
