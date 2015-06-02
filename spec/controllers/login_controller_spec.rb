bcf28a777f1cf5c64450940d85654f4b05d92b3d
jproyo <jproyo@mxhero.com>
2015-06-02 17:10:25 -0300

    NA: Adding tdd signup

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -14,8 +14,25 @@ RSpec.describe LoginController, type: :controller do
       assert_template 'login/index'
 			post :create, login: { email: 'unknown_user@test.com', password: 'password' }
 			assert_template 'login/index'
-			assert !flash.empty?
+			refute flash.empty?
 			assert_equal flash.now[:notice], 'User is not registered. Please click in Signup link'
+    end    
+  end
+
+ 	describe "GET #signup" do
+    it "go to signup page" do
+      get :signup
+      assert_template 'login/new'
+      response.should be_success
+      response.should have_http_status(200)
+    end
+
+    it "go to signup page and fill up form" do
+      get :signup
+      assert_template 'login/new'
+			post :signup, signup: { email: 'unknown_user@test.com', password: 'password' }
+			assert_template 'login/signup'
+			refute flash.empty?
     end    
   end
 
