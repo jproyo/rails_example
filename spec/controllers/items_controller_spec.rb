57643dccbcf3ec9ed243e6e1f87b03de25de4fc6
jproyo <jproyo@mxhero.com>
2015-06-03 12:29:49 -0300

    NA: Adding tdd uesr login

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,31 +2,25 @@ require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
 
-	def init_items
-		user = User.create!(username: "username@test.com", password: "mypass123")
-  	10.times do |i|
-  		Item.create!(link: "http://youtube.com/SDfafdaf#{i}", link_type: (i%2==0?"Youtube":"Image"), description: "My description #{i}", user: user)
-  	end
-  	user.id
-	end
+  fixtures :items, :users
 
   describe "Unauthenticated" do
     it "redirect to login page for unauthenticated user" do
       get :index
-      response.should have_http_status(302)
+      assert_redirected_to login_path
     end
   end
 
   describe "GET #index" do
 
     before(:each) do
-      controller.stub(:current_user).and_return(User.new)
+      controller.stub(:current_user).and_return(users(:useritems))
     end
 
 
     it "returns list of items for an user" do
-    	user_id = init_items
       get :index
+      assert_template 'items/index'
       response.should be_success
       response.should have_http_status(200)
     end
