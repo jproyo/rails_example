c1cd491a0ca7f6cbe43fabe362d88fa579155f32
jproyo <jproyo@mxhero.com>
2015-06-03 10:42:08 -0300

    NA: Adding tdd uesr login

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -2,13 +2,19 @@ require 'rails_helper'
 
 RSpec.describe LoginController, type: :controller do
 
-	describe "GET #login index" do
+  fixtures :users
+
+	describe "GET #login" do
     it "go to login page" do
       get :index
       response.should be_success
       response.should have_http_status(200)
     end
 
+       
+  end
+
+  describe "POST #login" do
     it "login with unexisting user" do
       get :index
       assert_template 'login/index'
@@ -16,8 +22,17 @@ RSpec.describe LoginController, type: :controller do
 			assert_template 'login/index'
 			refute flash.empty?
 			assert_equal flash.now[:notice], 'User is not registered. Please click in Signup link'
-    end    
-  end
+    end
 
+
+    it "login with existing user successfully" do
+      get :index
+      assert_template 'login/index'
+      user_prelogged = users(:prelogged)
+      post :create, login: { email: user_prelogged.username, password: 'mypassword123' }
+      assert flash.empty?
+      assert_redirected_to items_path
+    end 
+  end    
 
 end
